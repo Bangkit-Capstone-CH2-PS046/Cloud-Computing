a51eae12352327382a4b392dec51db3ede6ffcfb
RizkyHardian <trundul19@gmail.com>
2023-12-21 14:07:05 +0800

    p

--- login-api/config/database.js
+++ login-api/config/database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize({
     dialect: 'mysql',
     host: '34.101.47.161',
+    // host:'localhost',
     port: 3306,
     database: 'auth_db',
     username: 'root',
